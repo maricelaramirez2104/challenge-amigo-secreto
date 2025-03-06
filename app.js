8c314c3c98735698d852a65d7b10a7dd75e1c3e4
maricelaramirez2104 <maricelaramirez2104@gmail.com>
2025-03-06 15:13:39 -0600

    Crea la funcion renderizarAmigos();

--- app.js
+++ app.js
@@ -18,4 +18,23 @@ function agregarAmigo(){
     inputAmigo.value = "";
     //Se posiciona en el campo de texto
     inputAmigo.focus();
+    //agregamos la función renderizarAmigos() para que sea llamada cada que agregamos un nombre
+    renderizarAmigos();
+}
+
+function renderizarAmigos(){
+    //En esta función vamos agregar en la lista listaamigos los nombres de los amigos unicos
+    //que no se repitan los nombres que ya estan en el arreglo cuando se escriban en el campo de texto
+    let listaAmigos = document.getElementById("listaAmigos");
+    //Limpiamos la lista de amigos
+    listaAmigos.innerHTML = "";
+
+    //En este for vamos agregar todos los nombres a nuestra listaAmigos que tiene nuestro arreglo amigos[]
+    for(let i = 0; i < amigo.length; i++){
+        //crea un elemento li por cada nombre que se encuentra en mi arreglo amigo[]
+        let item = document.createElement("li");
+        item.textContent = amigo[i];
+        //llamamos a la Ul listaAmigos los hijos que se encuentra en cada li item
+        listaAmigos.appendChild(item);
+    }
 }
