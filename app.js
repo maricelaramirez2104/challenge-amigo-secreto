6f94231af7e6e9e85206ef9ca8b5ea6a85809498
maricelaramirez2104 <maricelaramirez2104@gmail.com>
2025-03-06 14:48:44 -0600

    Crea la función agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+
+function agregarAmigo(){
+    //en esta función vamos agregar en un arreglo los nombres que se escriban en el campo de texto
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    //si no se agregado un nombre manda una alerta
+    if (!nombreAmigo){
+        alert("Debes ingresar un nombre");
+        return;
+    }
+
+    //Agrega el nombre a la lista
+    amigo.push(nombreAmigo);
+    //Limpia el campo de texto
+    inputAmigo.value = "";
+    //se posiciona en el campo de texto
+    inputAmigo.focus();
+}
